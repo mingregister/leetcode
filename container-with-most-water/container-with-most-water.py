98d18036b449227b1b12e4de98c04ac396cac95b
mingregister <774262573@qq.com>
2020-04-18 02:12:25 +0800

    import annotations

--- container-with-most-water/container-with-most-water.py
+++ container-with-most-water/container-with-most-water.py
@@ -2,30 +2,32 @@
 # author: mingregister
 # date：2020-04-18,00:22 
 
+from __future__ import annotations
+
 # my answer, 超时了。
 # 我这里是只考虑了*单指针*
-class Solution:
-    def maxArea(self, height):
-        max = 0
-        length = len(height)
-        if length ==2:
-            return min(height)
+# class Solution:
+#     def maxArea(self, height):
+#         max = 0
+#         length = len(height)
+#         if length ==2:
+#             return min(height)
+# 
+#         for standard in reversed(range(length)):
+#             for i in range(length):
+#                 if height[i] >= height[standard]:
+#                     if height[standard]*(standard-i) > max:
+#                         max = height[standard]*(standard-i)
+#                         print("first height[standard]: %d, height[i]: %d, standard: %d, i: %d" %(height[standard],height[i], standard,i))
+#                         continue
+#                 else:
+#                     if height[i]*(standard-i) > max:
+#                         max = height[i]*(standard-i)
+#                         print("second height[standard]: %d, height[i]: %d, standard: %d, i: %d" %(height[standard],height[i], standard,i))
+#                         continue
+#         return max
 
-        for standard in reversed(range(length)):
-            for i in range(length):
-                if height[i] >= height[standard]:
-                    if height[standard]*(standard-i) > max:
-                        max = height[standard]*(standard-i)
-                        print("first height[standard]: %d, height[i]: %d, standard: %d, i: %d" %(height[standard],height[i], standard,i))
-                        continue
-                else:
-                    if height[i]*(standard-i) > max:
-                        max = height[i]*(standard-i)
-                        print("second height[standard]: %d, height[i]: %d, standard: %d, i: %d" %(height[standard],height[i], standard,i))
-                        continue
-        return max
-
-// 官方双指针方案
+# 官方双指针方案
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         l, r = 0, len(height) - 1
